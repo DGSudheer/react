9ea0fce5144b487096f2598d9d4b1bb5fec1d17e
D G Sudheer <dgsudheer@Ds-MacBook-Air.local>
2024-10-18 16:08:00 +0530

    render arrays and objects in props

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -1,12 +1,15 @@
 import './Product.css'
 
-function Product({title, name, price="1L"}){
-    // console.log(props);
+function Product({title, name, price="1L", features, basics}){
+    //console.log(basic.b);
     return(
         <div className="Product">
             <h3>Product Name: {name}</h3>
             <h5>Product Title: {title}</h5>
-            <h6>Product Description: Price is {price}</h6>
+            <h6>Product Description: Price is {price} 
+            Details are as follows - {features}
+            </h6>
+            <p>{basics}</p>
         </div>
     );
 }
