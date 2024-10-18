e6330030a1a46a4fb2a3ea006ed4349cbef2ab36
D G Sudheer <dgsudheer@Ds-MacBook-Air.local>
2024-10-18 23:44:34 +0530

    map and conditionals

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -1,15 +1,17 @@
 import './Product.css'
 
-function Product({title, name, price="1L", features, basics}){
+function Product({title, name, features, price="100000"}){
     //console.log(basic.b);
+    //const l = features.map((feature) => <li>{feature}</li>);
     return(
         <div className="Product">
             <h3>Product Name: {name}</h3>
             <h5>Product Title: {title}</h5>
-            <h6>Product Description: Price is {price} 
-            Details are as follows - {features}
+            <h6>Product Description: Price is {price} <br />
+            Details are as follows - {features.map((feature) => <li>{feature}</li>)}
+            <br />
+            {price>65000 ? "Discount of 5%" : <a href="/">Get Other Offers</a>}
             </h6>
-            <p>{basics}</p>
         </div>
     );
 }
