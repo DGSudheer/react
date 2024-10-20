b7c47caa60bec90a055b2aa2527c1981f355850e
D G Sudheer <dgsudheer@Ds-MacBook-Air.local>
2024-10-21 00:30:26 +0530

    warning fix

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -1,14 +1,12 @@
 import './Product.css'
 
-function Product({title, name, features, price="100000"}){
-    //console.log(basic.b);
-    //const l = features.map((feature) => <li>{feature}</li>);
+function Product({title, name, features, price}){
     return(
         <div className="Product">
             <h3>Product Name: {name}</h3>
             <h5>Product Title: {title}</h5>
             <h6>Product Description: Price is {price} <br />
-            Details are as follows - {features.map((feature) => <li>{feature}</li>)}
+            <ul> Details are as follows - {features.map((feature) => <li key={feature}>{feature}</li>)} </ul>
             <br />
             {price>65000 ? "Discount of 5%" : <a href="/">Get Other Offers</a>}
             </h6>
